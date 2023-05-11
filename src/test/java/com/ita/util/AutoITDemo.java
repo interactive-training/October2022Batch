6dc631d5c04164e9412a15fc41d2a059bba1b767
Pramod Mallick <pramod.mallick@gmail.com>
2023-05-11 22:35:13 +0100

    autoIT

---
+++ src/test/java/com/ita/util/AutoITDemo.java
@@ -0,0 +1,30 @@
+package com.ita.util;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class AutoITDemo {
+
+
+
+    @Test
+    public void uploadFile() throws IOException {
+
+        //login to app
+
+
+        //click upload button -- using selenium
+
+
+        //how to exeute EXE file
+        Runtime.getRuntime().exec("D:\\temp\\autoITUpload.exe");
+
+
+        //click upload file
+
+        //verify that 'success' msg appearfs
+
+
+    }
+}
