98730d10565124b9801cddd4c24d0b4b2c1c2fc5
PreetPankaj <preetpankaj15@gmail.com>
2024-05-16 19:02:59 +0100

    POM of MyAccountDashboard and Orders,TestRunner for OrdersRunner and MyAccountrunner,OrdersRunner and MyAccountRunner in testng.xml

--- src/test/java/TestRunner/HHT_MyAccountRunner.java
+++ src/test/java/TestRunner/HHT_MyAccountRunner.java
@@ -8,22 +8,21 @@ import org.testng.annotations.DataProvider;
         features= "src/test/java/HHT_Features",
         glue = "HHT_Steps",
         monochrome = true,
-
-        tags = "@CreateNews or @EditNews or @ViewNews or @DeleteNews",
-
+        //publish = true,
+        tags = "@familyMembers",//"@Calendar",//"@Events","//@Poojas",//"@Donations",
 
         plugin = {"pretty", "html:target/cucumber-reports.html",
                 "json:target/cucumber.json",
-                "rerun:target/failed_scenarios.txt",
+//                "rerun:target/failed_scenarios.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
                 // "com.aventstack:extentreports-cucumber7-adapter:${1.14.0}"
         }
 )
-public class HHT_NewsRunner extends AbstractTestNGCucumberTests {
+public class HHT_MyAccountRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios(){
         return super.scenarios();
     }
-}
 
+}
